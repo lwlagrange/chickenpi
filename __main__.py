bc8944e4777767f293fb906ec001d187db142a92
laurence <lwlagrange@gmail.com>
2020-05-10 21:27:09 -0600

    filter sensor data using simple moving average

--- __main__.py
+++ __main__.py
@@ -7,46 +7,40 @@ from sensor import realtime as rtc
 
 # main init
 if __name__ == '__main__':
+
+    def button_pushed(channel):
+        print "rising edge detected on channel: " + str(channel) + "\n"
+
     GPIO.setwarnings(False)  # Ignore warning for now
     GPIO.setmode(GPIO.BOARD)  # setup IO pin mode
-    pins = {'button': 15, 'open': 31, 'close': 33, 'cool': 35, 'heat': 37}  # create a dictionary of the pin assignments
+    outputs = {'open': 31, 'close': 33, 'cool': 35, 'heat': 37}  # create a dictionary of output pin assignments
+    inputs = {'button': 15}  # create a dictionary of input pin assignments
     light = 0  # light variables
     upperLux = 20
 
     # temp variables
     temp = 0
-    upperTemp = 25
-    lowerTemp = 22
+    upperTemp = 20
+    lowerTemp = 18
+    nominalTemp = int(lowerTemp + ((upperTemp - lowerTemp) / 2))
 
     # iterate through the list of pins
-    for pin in pins.values():
-        GPIO.setup(pin, GPIO.OUT)
+    for pin_out in outputs.values():
+        GPIO.setup(pin_out, GPIO.OUT)
+
+    buttonState = 0
+
+    GPIO.setup(inputs['button'], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(inputs['button'], GPIO.RISING, callback=button_pushed, bouncetime=300)
 
     while True:
         # get the current temp
         temp = env.read_temp()
-        # get the current light level
-        # light = lux.read_light()
 
         if temp < lowerTemp:
             print 'heating..\n'
-            GPIO.output(pins['heat'], 1)
+            print 'nominal temp is: ' + str(nominalTemp) + '\n'
+            GPIO.output(outputs['heat'], 1)
         else:
-            GPIO.output(pins['heat'], 0)
-
-        if temp > upperTemp:
-            print 'cooling.. \n'
-            GPIO.output(pins['cool'], 1)
-        else:
-            GPIO.output(pins['cool'], 0)
-
-        if light > upperLux:
-            GPIO.output(pins['open'], 1)
-            GPIO.output(pins['close'], 1)
-
-        else:
-            GPIO.output(pins['open'], 0)
-            GPIO.output(pins['close'], 0)
-
-        # Updated every 30 sec
+            GPIO.output(outputs['heat'], 0)
         time.sleep(2)
